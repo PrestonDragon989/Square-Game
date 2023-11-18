89a2dbac684478f2ab9f4678a13e0559d78763ae
PrestonDragon <122578013+PrestonDragon989@users.noreply.github.com>
2023-11-18 15:19:56 -0800

    Create collision.js

---
+++ collision.js
@@ -0,0 +1,18 @@
+//Collision Handling
+class Collision {
+    constructor(Player) {
+        //Getting Player
+        this.player = Player;
+
+        //Utils
+        this.utils = new Utils();
+    }
+
+    playerCollision(canvas) {
+        //Checking Wall Collisions
+        if (this.player.x < 0) this.player.x = 0;
+        if (this.player.x > canvas.width - this.player.width) this.player.x = canvas.width - this.player.width;
+        if (this.player.y < 0) this.player.y = 0;
+        if (this.player.y > canvas.height - this.player.height) this.player.y = canvas.height - this.player.height;
+    }
+}
